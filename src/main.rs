a2e54f489b9da539b1633e624c83dad84fe09000
Roni Palva-aho <roni.palva-aho@pinja.com>
2022-01-30 15:00:37 +0200

    Static bundling & serve all

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 mod windows;
 
+use rust_embed::RustEmbed;
 use simple_logger::SimpleLogger;
 use warp::{hyper::StatusCode, Filter, Rejection, Reply};
+
+#[derive(RustEmbed)]
+#[folder = "www/"]
+struct Asset;
 
 #[derive(Copy, Clone)]
 pub enum VkCodes {
@@ -12,22 +17,22 @@ pub enum VkCodes {
 async fn main() {
     SimpleLogger::new().init().unwrap();
 
-    let index = warp::path!().and(warp::fs::dir("www"));
+    let index = warp::path!().map(|| warp::reply::html(get_index()));
     let trigger = warp::path!("trigger").and_then(do_keypress);
     let routes = warp::get().and(index).or(warp::post().and(trigger));
 
-    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+    warp::serve(routes).run(([0, 0, 0, 0], 3030)).await;
 }
 
 async fn do_keypress() -> Result<impl Reply, Rejection> {
     let status = match windows::keypress(&VkCodes::Space) {
         2 => {
             log::info!("Succesfully inputted events to stream");
-            StatusCode::INTERNAL_SERVER_ERROR
+            StatusCode::OK
         }
         1 => {
             log::warn!("Sending either UP or DOWN failed");
-            StatusCode::OK
+            StatusCode::INTERNAL_SERVER_ERROR
         }
         _ => {
             log::error!("Could not push key events");
@@ -35,4 +40,10 @@ async fn do_keypress() -> Result<impl Reply, Rejection> {
         }
     };
     Ok(status)
+}
+
+fn get_index() -> String {
+    let file = Asset::get("index.html").unwrap();
+    let text = std::str::from_utf8(file.data.as_ref()).unwrap();
+    text.to_string()
 }
